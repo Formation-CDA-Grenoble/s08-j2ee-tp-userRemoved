f097c87957bbcfd5b841b2ef950b9ec0bcfcd62d
userRemoved <leo.hirigoyen@gmail.com>
2020-02-07 14:03:56 +0100

    FD

--- api/src/test/java/com/example/api/controller/UserController.java
+++ api/src/test/java/com/example/api/controller/UserController.java
@@ -1,9 +1,12 @@
 
-package com.example.api.controller ; 
+package com.example.api.controller;
+
 import javax.validation.Valid;
 import java.util.List;
 import com.example.api.model.User;
 import com.example.api.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -16,16 +19,16 @@ import org.springframework.web.server.ResponseStatusException;
 @CrossOrigin
 public class UserController {
 
-    
+    @Autowired 
     private UserRepository userRepository;
     
-   
+    @GFDGFDGFDGFDGFDGDFG
     @GetMapping("")
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
-  
+    @GFDGFDGFDGFDGFDGDFG
     @PostMapping("")
   
     @ResponseStatus(value = HttpStatus.CREATED)
@@ -34,26 +37,26 @@ public class UserController {
         return userRepository.save(user);
     }
 
-    
+    @GFDGFDGFDGFDGFDGDFG
     @PutMapping("/{id}")
     public User updateUser(@PathVariable(value = "id") Long userId, @Valid @RequestBody User newUser) {
         
         User user = this.fetchUser(userId);
-       
+      
         user.setEmail(newUser.getEmail());
         user.setName(newUser.getName());
 
         return userRepository.save(user);
-    }
-
+    } 
+    @GFDGFDGFDGFDGFDGDFG
     @GetMapping("/{id}")
     public User getUserById(@PathVariable(value = "id") Long userId) {
         return this.fetchUser(userId);
-    }
+    }SDFSFSDFS
 
-   
+  
     @DeleteMapping("/{id}")
-   
+  
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void deleteUser(@PathVariable(value = "id") Long userId) {
         User user = this.fetchUser(userId);
